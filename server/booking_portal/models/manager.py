7cd27e0e669bdb9157ccd286a11ba6dadf77358a
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-01-13 22:16:53 +0530

    refactor: change to alternative function in 4.x.x

--- server/booking_portal/models/manager.py
+++ server/booking_portal/models/manager.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import gettext_lazy
 
 
 class CustomUserManager(BaseUserManager):
 
     def create_user(self, email, password, name, **extra_fields):
         if not email:
-            raise ValueError(ugettext_lazy("Email cannot be null"))
+            raise ValueError(gettext_lazy("Email cannot be null"))
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
@@ -23,9 +23,9 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(ugettext_lazy(
+            raise ValueError(gettext_lazy(
                 'Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(ugettext_lazy(
+            raise ValueError(gettext_lazy(
                 'Superuser must have is_superuser=True.'))
         return self.create_user(email, password, name, **extra_fields)
