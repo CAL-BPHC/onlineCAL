4357c0fd2490d697614ae42c7a5ec9c069d7d2c6
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-01-26 15:18:29 +0530

    chore: add debug print statements to find the time taken to send emails
    this is an attempt to find max time taken to send emails with which proper timeout time can be set

--- server/booking_portal/management/commands/sendemails.py
+++ server/booking_portal/management/commands/sendemails.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
+import time
 
 from ...mail import send_mass_html_mail
 from ...models.email import EmailModel
@@ -11,7 +12,12 @@ class Command(BaseCommand):
     help = 'Send all queued emails'
 
     def handle(self, *args, **options):
+        print('Sleeping for 60 seconds')
+        time.sleep(60)
+        print('Slept for 60 seconds')
         emails = EmailModel.objects.filter(sent=False).order_by('date_time')[:MAX_EMAIL_PER_COMMAND]
+        print(f'Sending {len(emails)} emails')
+        start = time.time()
         datatuple = []
         for email in emails:
             message = (
@@ -21,10 +27,17 @@ class Command(BaseCommand):
                 settings.EMAIL_HOST_USER,
                 [email.receiver],
             )
+            print(email.receiver)
             datatuple.append(message)
             email.sent = True
         send_mass_html_mail(datatuple, fail_silently=False)
 
+        end = time.time()
+        elapsed = end - start
+        print(f'Sent {len(emails)} emails in {elapsed:.2f} seconds')
+
+        start = time.time()
         EmailModel.objects.bulk_update(emails, ['sent'])
-
-
+        end = time.time()
+        elapsed = end - start
+        print(f'Updated {len(emails)} emails in {elapsed:.2f} seconds')
