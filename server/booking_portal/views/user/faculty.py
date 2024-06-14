19f4908433ead387cfcbd0c8f2eb24add9b2b7cf
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-06-14 15:22:00 +0530

    style: format using ruff and remove unused exceptions

--- server/booking_portal/views/user/faculty.py
+++ server/booking_portal/views/user/faculty.py
@@ -15,21 +15,21 @@ def faculty_portal(request):
     f = BasePortalFilter(
         request.GET,
         queryset=models.Request.objects.filter(faculty=request.user)
-                                       .select_related('slot')
-                                       .order_by('-slot__date')
+        .select_related("slot")
+        .order_by("-slot__date"),
     )
     page_obj = f.paginate()
 
     return render(
         request,
-        'booking_portal/portal_forms/base_portal.html',
+        "booking_portal/portal_forms/base_portal.html",
         {
-            'page_obj': page_obj,
-            'filter_form': f.form,
-            'user_type': 'faculty',
-            'user_is_student': False,
-            'modifiable_request_status': models.Request.WAITING_FOR_FACULTY,
-        }
+            "page_obj": page_obj,
+            "filter_form": f.form,
+            "user_type": "faculty",
+            "user_is_student": False,
+            "modifiable_request_status": models.Request.WAITING_FOR_FACULTY,
+        },
     )
 
 
@@ -39,18 +39,19 @@ def faculty_request_accept(request, id):
     try:
         with transaction.atomic():
             request_object = models.Request.objects.get(
-                id=id,
-                status=models.Request.WAITING_FOR_FACULTY)
+                id=id, status=models.Request.WAITING_FOR_FACULTY
+            )
             faculty = request_object.faculty
-            if (faculty == models.Faculty.objects.get(id=request.user.id)):
+            if faculty == models.Faculty.objects.get(id=request.user.id):
                 request_object.status = models.Request.WAITING_FOR_LAB_ASST
                 request_object.lab_assistant = random.choice(
-                    models.LabAssistant.objects.all())
+                    models.LabAssistant.objects.all()
+                )
                 request_object.save()
-                return redirect('faculty_portal')
+                return redirect("faculty_portal")
             else:
                 return HttpResponse("Bad Request")
-    except Exception as e:
+    except Exception:
         raise Http404("Page Not Found")
 
 
@@ -60,14 +61,14 @@ def faculty_request_reject(request, id):
     try:
         with transaction.atomic():
             request_object = models.Request.objects.get(
-                id=id,
-                status=models.Request.WAITING_FOR_FACULTY)
+                id=id, status=models.Request.WAITING_FOR_FACULTY
+            )
             faculty = request_object.faculty
-            if (faculty == models.Faculty.objects.get(id=request.user.id)):
+            if faculty == models.Faculty.objects.get(id=request.user.id):
                 request_object.status = models.Request.REJECTED
                 request_object.save()
-                return redirect('faculty_portal')
+                return redirect("faculty_portal")
             else:
                 return HttpResponse("Bad Request")
-    except Exception as e:
+    except Exception:
         raise Http404("Page Not Found")
