4db6f2fa39db769171b90ef3adf3d25cf312965f
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-06-14 16:43:20 +0530

    style: format using ruff

--- server/booking_portal/views/user/lab_assistant.py
+++ server/booking_portal/views/user/lab_assistant.py
@@ -11,22 +11,21 @@ from ... import models, permissions
 @user_passes_test(permissions.is_lab_assistant)
 def lab_assistant_portal(request):
     f = BasePortalFilter(
-        request.GET,
-        queryset=models.Request.objects.order_by('-slot__date', '-pk')
+        request.GET, queryset=models.Request.objects.order_by("-slot__date", "-pk")
     )
     page_obj = f.paginate()
 
     return render(
         request,
-        'booking_portal/portal_forms/base_portal.html',
+        "booking_portal/portal_forms/base_portal.html",
         {
-            'page_obj': page_obj,
-            'nav_range': get_pagintion_nav_range(page_obj),
-            'filter_form': f.form,
-            'user_type': 'assistant',
-            'user_is_student': False,
-            'modifiable_request_status': models.Request.WAITING_FOR_LAB_ASST,
-        }
+            "page_obj": page_obj,
+            "nav_range": get_pagintion_nav_range(page_obj),
+            "filter_form": f.form,
+            "user_type": "assistant",
+            "user_is_student": False,
+            "modifiable_request_status": models.Request.WAITING_FOR_LAB_ASST,
+        },
     )
 
 
@@ -36,15 +35,15 @@ def lab_assistant_accept(request, id):
     try:
         with transaction.atomic():
             request_object = models.Request.objects.get(
-                id=id,
-                status=models.Request.WAITING_FOR_LAB_ASST
+                id=id, status=models.Request.WAITING_FOR_LAB_ASST
             )
             request_object.lab_assistant = models.LabAssistant.objects.get(
-                id=request.user.id)
+                id=request.user.id
+            )
             request_object.status = models.Request.APPROVED
             request_object.save()
-            return redirect('lab_assistant')
-    except:
+            return redirect("lab_assistant")
+    except Exception:
         raise Http404("Page Not Found")
 
 
@@ -53,11 +52,9 @@ def lab_assistant_accept(request, id):
 @user_passes_test(permissions.is_lab_assistant)
 def lab_assistant_reject(request, id):
     request_object = models.Request.objects.get(
-        id=id,
-        status=models.Request.WAITING_FOR_LAB_ASST
+        id=id, status=models.Request.WAITING_FOR_LAB_ASST
     )
-    request_object.lab_assistant = models.LabAssistant.objects.get(
-        id=request.user.id)
+    request_object.lab_assistant = models.LabAssistant.objects.get(id=request.user.id)
     request_object.status = models.Request.REJECTED
     request_object.save()
-    return redirect('lab_assistant')
+    return redirect("lab_assistant")
