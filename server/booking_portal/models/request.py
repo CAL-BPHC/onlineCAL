8fbf46628dd97273135ee82c053885b8212c069c
okaybro <66475772+Chaitanya-Keyal@users.noreply.github.com>
2026-09-15 03:51:42 +0530

    feat: address department emails to the HOD
    Department accounts are named after the department in upper case, so
    the emails they received opened with "Dear PHARMACY,". Add a salutation
    property on the user models and use it for the recipient name. Faculty
    and students keep their name; departments get "HOD of Pharmacy
    Department".

--- server/booking_portal/models/request.py
+++ server/booking_portal/models/request.py
@@ -290,7 +290,7 @@ def send_email_after_save(sender, instance, **kwargs):
         text = render_to_string(
             "email/department_pending.txt",
             {
-                "recipient_name": instance.faculty.department.name,
+                "recipient_name": instance.faculty.department.salutation,
                 "student_name": instance.student.name,
                 "instrument_name": instance.instrument.name,
                 "slot": instance.slot.description,
@@ -300,7 +300,7 @@ def send_email_after_save(sender, instance, **kwargs):
         text_html = render_to_string(
             "email/department_pending.html",
             {
-                "recipient_name": instance.faculty.department.name,
+                "recipient_name": instance.faculty.department.salutation,
                 "student_name": instance.student.name,
                 "instrument_name": instance.instrument.name,
                 "slot": instance.slot.description,
